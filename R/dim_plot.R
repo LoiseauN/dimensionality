c9e9aee6f9df5abb5ee10bb759f149109ccf4c77
Nicolas Casajus <nicolas.casajus@gmail.com>
2020-11-28 09:43:32 +0100

    :bug: Remove metric argument

--- R/dim_plot.R
+++ R/dim_plot.R
@@ -2,16 +2,16 @@
 #'
 #' @param data_miss __ADD DESCRIPTION__
 #' @param dim_pcoa __ADD DESCRIPTION__
-#' @param metric __ADD DESCRIPTION__
 #'
 #' @export
+#' @importFrom grid gpar textGrob
 #' @importFrom gridExtra grid.arrange
 
-dim_plot <- function(data_miss, dim_pcoa, metric) {
+dim_plot <- function(data_miss, dim_pcoa) {
 
-  if (metric == "MAD") {
+  # if (metric == "MAD") {
 
-    gplot <- ggplot(data_miss, aes(x = dim, y = MAD)) +
+    gplot1 <- ggplot(data_miss, aes(x = dim, y = MAD)) +
       stat_summary(fun = "mean", geom = "line", color = "dodgerblue1", size = 1.3,
                    alpha = 0.4) +
       stat_summary(fun = "mean", color = "dodgerblue1", size = 1) +
@@ -20,9 +20,11 @@ dim_plot <- function(data_miss, dim_pcoa, metric) {
       labs(y = "MAD") +
       scale_x_continuous(limits = c(0, dim_pcoa), breaks = seq(1, dim_pcoa, 1))
 
-  } else {
+  # }
 
-    gplot <- ggplot(data_miss, aes(x = dim, y = AUC)) +
+  # if (metric == "AUC") {
+
+    gplot2 <- ggplot(data_miss, aes(x = dim, y = AUC)) +
       stat_summary(fun = "mean", geom = "line", color = "chartreuse1", size = 1.3,
                    alpha = 0.4) +
       stat_summary(fun = "mean", color = "chartreuse1", size = 1) +
@@ -30,10 +32,10 @@ dim_plot <- function(data_miss, dim_pcoa, metric) {
       labs(x = "Number of dimensions") +
       labs(y = "AUC") +
       scale_x_continuous(limits = c(0, dim_pcoa), breaks = seq(1, dim_pcoa, 1))
-  }
+  # }
 
-  gridExtra::grid.arrange(gplot, ncol = 1,
-    top  = textGrob("Influence of number of dimensions considered",
-    gp   = gpar(fontsize = 20, font = 3))
+  gridExtra::grid.arrange(gplot1, gplot2, ncol = 2,
+    top  = grid::textGrob("Influence of number of dimensions considered",
+    gp   = grid::gpar(fontsize = 20, font = 3))
   )
 }
